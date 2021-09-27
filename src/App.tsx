7e3ed7589ca0300ccc1eaa9ecc07b475020133e6
CommentatoreVideo <54153405+CommentatoreVideo@users.noreply.github.com>
2021-09-27 23:20:03 +0200

    Aggiunto firebase

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,65 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 import Grafici from "./Grafici";
-import { getMemoria } from "./memoria";
-import { useState } from "react";
+import { createIndexMemoria } from "./memoria";
+import { useEffect, useState } from "react";
 import Selettori from "./Selettori";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { Memoria } from "./interfaces";
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const App = () => {
-	const memoria = getMemoria();
-  const calcolaDomandeDefault:Array<[number,string,boolean]> =memoria.domande.map(domanda=>[domanda.indice,domanda.nome,true]);
-  const calcolaOffertaDefault:Array<[number,string,boolean]> =memoria.offerte.map(offerte=>[offerte.indice,offerte.nome,true]);
-  const calcolaDefault:Array<[number,string,boolean]> =[...calcolaDomandeDefault,...calcolaOffertaDefault];
-  const [calcola,setCalcola]=useState<Array<[number,string,boolean]>>(calcolaDefault);
+	const [loading,setLoading]=useState(true);
+	const [memoria, setMemoria] = useState<Memoria>({ domande: [], offerte: [] });
+	useEffect(function () {
+		const fetchData = async () => {
+			// database
+			const db = firebase.firestore();
+			// data
+			let dataDomande = await db.collection("domande").get();
+			const domande = dataDomande.docs.map(doc => {
+				return {
+					coppie: doc.data().coppie.map((coppia: string) => [Number(coppia.split(",")[0]), Number(coppia.split(",")[1])]) as Array<[number, number]>,
+					nome: doc.data().nome as string,
+					prova: true,
+				};
+			});
+			let dataOfferte = await db.collection("offerte").get();
+			const offerte = dataOfferte.docs.map(doc => {
+				return {
+					coppie: doc.data().coppie.map((coppia: string) => [Number(coppia.split(",")[0]), Number(coppia.split(",")[1])]) as Array<[number, number]>,
+					nome: doc.data().nome as string,
+				};
+			});
+			setMemoria(createIndexMemoria({ offerte, domande }));
+			setLoading(false);
+		};
+		// start loading data
+		fetchData();
+	}, []);
+
+	const [calcola, setCalcola] = useState<Array<[number, string, boolean]>>([]);
+	useEffect(function () {
+		const calcolaDomandeDefault: Array<[number, string, boolean]> = memoria.domande.map(domanda => [domanda.indice, domanda.nome, true]);
+		const calcolaOffertaDefault: Array<[number, string, boolean]> = memoria.offerte.map(offerte => [offerte.indice, offerte.nome, true]);
+		const calcolaDefault: Array<[number, string, boolean]> = [...calcolaDomandeDefault, ...calcolaOffertaDefault];
+		setCalcola(calcolaDefault);
+	}, [memoria]);
+	if(loading) return <h1>Caricamento...</h1>
 	return (
 		<div className="container-fluid">
-      <Selettori memoria={memoria} calcola={calcola} setCalcola={setCalcola}></Selettori>
+			<Selettori memoria={memoria} calcola={calcola} setCalcola={setCalcola}></Selettori>
 			<Grafici memoria={memoria} calcola={calcola}></Grafici>
 		</div>
 	);
--- src/memoria.tsx
+++ src/memoria.tsx
@@ -1,68 +1,6 @@
 import { Calcola, DomandaOfferta, Memoria, MemoriaRaw } from "./interfaces";
 
-const memoria: MemoriaRaw = {
-	domande: [
-		{
-			nome: "Alice",
-			coppie: [
-				[12, 0],
-				[8, 0],
-				[7, 1],
-				[6, 2],
-				[4, 4],
-				[1, 6],
-			],
-		},
-		{
-			nome: "Luca",
-			coppie: [
-				[12, 0],
-				[8, 2],
-				[7, 3],
-				[6, 5],
-				[4, 6],
-				[1, 8],
-			],
-		},
-		{
-			nome: "Francesca",
-			coppie: [
-				[12, 1],
-				[8, 2],
-				[7, 4],
-				[6, 5],
-				[4, 7],
-				[1, 8],
-			],
-		},
-	],
-	offerte: [
-		{
-			nome: "Livia",
-			coppie: [
-				[12, 11],
-				[8, 7],
-				[7, 4],
-				[6, 3],
-				[4, 2],
-				[1, 1],
-			],
-		},
-		{
-			nome: "Antonio",
-			coppie: [
-				[12, 9],
-				[8, 6],
-				[7, 4],
-				[6, 2],
-				[4, 1],
-				[1, 0],
-			],
-		},
-	],
-};
-
-export function getMemoria(): Memoria {
+export function createIndexMemoria(memoria:MemoriaRaw): Memoria {
 	const risultato: Memoria = { domande: [], offerte: [] };
 	risultato.domande = memoria.domande.slice().map((domanda, indice) => {
 		return { ...domanda, indice };
