90abe4f6fa17198b73addcf99891bc0c33eda74d
CommentatoreVideo <54153405+CommentatoreVideo@users.noreply.github.com>
2021-09-28 06:46:43 +0200

    Aggiunte tabelle dati

--- src/Grafici.tsx
+++ src/Grafici.tsx
@@ -4,11 +4,54 @@ import ScatterChartMulti from "./Components/ScatterChartMulti";
 import { Calcola, Dataset, DomandaOfferta, GraficiProps } from "./interfaces";
 import { getDomandeOfferteValide } from "./memoria";
 
-function generaColGrafico(label: string, data: Array<{ x: number; y: number }>,key:number) {
-	return <div key={key} className="col-xl-4">{<ScatterChart data={data}>{label}</ScatterChart>}</div>;
+function generaColGrafico(label: string, data: Array<{ x: number; y: number }>, key: number) {
+	return (
+		<div key={key} className="col-xl-4">
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Prezzo</th>
+						<th>Quantità</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map(({ x, y },index) => (
+						<tr key={index}>
+							<td>{x}</td>
+							<td>{y}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			{<ScatterChart data={data}>{label}</ScatterChart>}
+		</div>
+	);
 }
-function generaColGraficoTotale(datasets: Array<Dataset>,key:number) {
-	return <div key={key} className="col-xl-4">{<ScatterChartMulti datasets={datasets}></ScatterChartMulti>}</div>;
+function generaColGraficoTotale(datasets: Array<Dataset>, key: number) {
+	const totale = datasets.find(dataset => dataset.label === "Totale");
+	return (
+		<div key={key} className="col-xl-4">
+			{totale !== undefined ? (
+				<table className="table">
+					<thead>
+						<tr>
+							<th>Prezzo</th>
+							<th>Quantità</th>
+						</tr>
+					</thead>
+					<tbody>
+						{totale?.data.map(({ x, y },index) => (
+							<tr key={index}>
+								<td>{x}</td>
+								<td>{y}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : null}
+			{<ScatterChartMulti datasets={datasets}></ScatterChartMulti>}
+		</div>
+	);
 }
 
 export function creaDataset(label: string, coppie: Array<[number, number]>) {
@@ -23,7 +66,7 @@ function filtraDatasets(datasetsTotale: Array<[Dataset, number]>, calcolare: Cal
 	return datasetsTotale
 		.filter(([dataset, indice]) => {
 			if (dataset.label === "Totale") return true;
-			return calcolare.find(([id]) => id === indice)?.[2]||false;
+			return calcolare.find(([id]) => id === indice)?.[2] || false;
 		})
 		.map(([dataset]) => dataset);
 }
@@ -37,35 +80,44 @@ function generaGrafici(memoria: DomandaOfferta[], vettore: any[], calcola: Calco
 	const datasetsEIndiciTotale = calcolaDatasetsTotale(memoria, calcola);
 	const datasetFiltrati = filtraDatasets(datasetsEIndiciTotale, calcola);
 	const datasetsTotale = datasetsEIndiciTotale.map(([dataset]) => dataset);
-	let i=0;
+	let i = 0;
 	for (const { label, data } of datasetsTotale) {
-		const col = label === "Totale" ? generaColGraficoTotale(datasetFiltrati,i++) : generaColGrafico(label, data,i++);
+		const col = label === "Totale" ? generaColGraficoTotale(datasetFiltrati, i++) : generaColGrafico(label, data, i++);
 		if (riga.length === 3) {
-			vettore.push(<div key={i++} className="row">{riga.slice()}</div>);
+			vettore.push(
+				<div key={i++} className="row">
+					{riga.slice()}
+				</div>
+			);
 			riga = [];
 		}
 		riga.push(col);
 	}
-	if (riga.length !== 0) vettore.push(<div key={i++} className="row">{riga.slice()}</div>);
+	if (riga.length !== 0)
+		vettore.push(
+			<div key={i++} className="row">
+				{riga.slice()}
+			</div>
+		);
 }
 
 function generaDatasetGraficoTotale(domande: Array<[number, number]>, offerte: Array<[number, number]>) {
 	return [creaDataset("Domande", domande), creaDataset("Offerte", offerte)];
 }
 
-function generaGraficoTotale(domandaTotale: Array<[number, number]>, offertaTotale: Array<[number, number]>,key:number) {
+function generaGraficoTotale(domandaTotale: Array<[number, number]>, offertaTotale: Array<[number, number]>, key: number) {
 	const datasets = generaDatasetGraficoTotale(domandaTotale, offertaTotale);
-	return <div className="row">{generaColGraficoTotale(datasets,key)}</div>;
+	return <div className="row">{generaColGraficoTotale(datasets, key)}</div>;
 }
 
-const Grafici: React.FunctionComponent<GraficiProps> = ({ memoria:{domande,offerte}, calcola }) => {
+const Grafici: React.FunctionComponent<GraficiProps> = ({ memoria: { domande, offerte }, calcola }) => {
 	const graficiDomande: any[] = [];
 	const graficiOfferte: any[] = [];
 	generaGrafici(domande, graficiDomande, calcola);
 	generaGrafici(offerte, graficiOfferte, calcola);
-	const domandeFiltrate=getDomandeOfferteValide(domande,calcola);
-	const offerteFiltrate=getDomandeOfferteValide(offerte,calcola);
-	const graficoFinale = generaGraficoTotale(getTotale(domandeFiltrate), getTotale(offerteFiltrate),-1);
+	const domandeFiltrate = getDomandeOfferteValide(domande, calcola);
+	const offerteFiltrate = getDomandeOfferteValide(offerte, calcola);
+	const graficoFinale = generaGraficoTotale(getTotale(domandeFiltrate), getTotale(offerteFiltrate), -1);
 	return (
 		<div>
 			<h1>Domande</h1>
